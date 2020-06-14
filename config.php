93895fccbb4b302ad7445db20bf7dd743801e2fd
arkabandyopadhyay <arkabandyopadhyay@outlook.com>
2020-06-14 18:25:05 +0530

    init

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$db_host	    = 'localhost';
+$db_user        = 'root';
+$db_password	= '4188000';
+$db_name	    = 'nbill';
+define('APP_URL', 'http://localhost:88/nbill');
+$_app_stage = 'Live'; // You can set this variable Live to Dev to enable ibilling Debug
---
+++ controllers/api.php
@@ -0,0 +1,156 @@
+<?php
+$data = array();
+$data['success'] = false;
+$data['msg'] = '';
+$ip = '';
+$ref = '';
+$agent = '';
+if(isset($_SERVER['REMOTE_ADDR'])){
+$ip = $_SERVER['REMOTE_ADDR'];
+}
+if(isset($_SERVER['HTTP_REFERER'])){
+    $ref = $_SERVER['HTTP_REFERER'];
+}
+if(isset($_SERVER['HTTP_USER_AGENT'])){
+    $agent = $_SERVER['HTTP_USER_AGENT'];
+}
+$data['ip'] = $ip;
+$data['ref'] = $ref;
+$data['agent'] = $agent;
+function api_response($data=array()){
+
+    header('Content-type: application/json');
+    echo json_encode( $data );
+exit;
+}
+
+if(isset($_POST['key']) AND ($_POST['key'] != '')){
+
+    $key = $_POST['key'];
+
+    $a = ORM::for_table('sys_api')->where('apikey',$key)->find_one();
+    if($a){
+        $data['success'] = true;
+
+        $method = _post('method');
+
+        switch ($method) {
+
+
+
+            case 'get_contact_details_by_id':
+
+               $id = _post('id');
+
+                $d = ORM::for_table('crm_accounts')->where('id',$id)->limit(1)->find_array();
+               if($d){
+                   $data['result'] = $d;
+                   api_response($data);
+               }
+               else{
+                   $data['result'] = false;
+                   $data['msg'] = 'Contact Not Found';
+                   api_response($data);
+               }
+
+                break;
+
+
+            case 'get_contact_details_by_email':
+
+                $email = _post('email');
+
+                $d = ORM::for_table('crm_accounts')->where('email',$email)->limit(1)->find_array();
+                if($d){
+                    $data['result'] = $d;
+                    api_response($data);
+                }
+                else{
+                    $data['result'] = false;
+                    $data['msg'] = 'Contact Not Found';
+                    api_response($data);
+                }
+
+                break;
+
+            case 'get_contacts':
+
+
+                $d = ORM::for_table('crm_accounts')->find_array();
+
+                $data['result'] = $d;
+
+                api_response($data);
+
+                break;
+
+
+            case 'add_contact':
+
+
+               $v = array();
+
+
+               $v['account'] = _post('account');
+               $v['email'] = _post('email');
+               $v['phone'] = _post('phone');
+               $v['address'] = _post('address');
+               $v['city'] = _post('city');
+               $v['zip'] = _post('zip');
+               $v['state'] = _post('state');
+               $v['country'] = _post('country');
+               $v['company'] = _post('company');
+               $v['password'] = _post('password');
+
+                $data['result'] = Contacts::add($v);
+
+
+                api_response($data);
+
+                break;
+
+
+
+            case 'get_invoices_by_contact_email':
+
+                $email = _post('email');
+                $cid = '';
+                $i = false;
+
+                $d = ORM::for_table('crm_accounts')->where('email',$email)->find_one();
+
+                if($d){
+                    $cid = $d['id'];
+                    $i = ORM::for_table('sys_invoices')->where('userid',$d['id'])->find_array();
+                }
+
+                $data['result'] = $i;
+                $data['email'] = $email;
+                $data['cid'] = $cid;
+
+                api_response($data);
+
+                break;
+
+
+
+            default:
+                $data['msg'] = 'Method Not Found';
+                api_response($data);
+        }
+
+    }
+    else{
+        $data['msg'] = 'Invalid API Key';
+        api_response($data);
+    }
+
+}
+else{
+    $data['msg'] = 'API Key is required';
+
+    api_response($data);
+
+}
+
+
